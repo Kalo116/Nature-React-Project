1691d863a2e4c65cb25cf79d0236d265f7eff0c0
Kalo116 <kaloyan0102@gmail.com>
2022-08-12 12:44:31 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
         <h1>I made a change, time to commit</h1>
+        <h2>I made another change, time for second commit</h2>
     </div>
   );
 }
